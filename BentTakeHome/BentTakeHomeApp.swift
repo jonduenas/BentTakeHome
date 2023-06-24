a5996b8584d23da6463716e46d27671a9cffa964
Jon Duenas <51519363+jonduenas@users.noreply.github.com>
2023-06-24 12:30:46 -0400

    Initial Commit

---
+++ BentTakeHome/BentTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BentTakeHomeApp.swift
+//  BentTakeHome
+//
+//  Created by Jon Duenas on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct BentTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
